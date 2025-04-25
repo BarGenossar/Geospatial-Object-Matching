b62b1ee1d792761340cda3010e1170344ac620a3
BarGenossar <bargenossar@gmail.com>
2025-04-25 15:08:30 +0300

    update experiment coordinator file

--- run_experiments.sh
+++ run_experiments.sh
@@ -1,22 +1,30 @@
 #!/bin/bash
 
+
 bkafi_criterion=("feature_importance")
-dataset_sizes=("small" "medium" "large")
-# normalizations=("True" "False")
+dataset_sizes=("small")
 normalizations=("True")
-# sdr_factors=("True" "False")
 sdr_factors=("False")
-eval_mode="blocking"
+eval_mode="matching"
 
 for criterion in "${bkafi_criterion[@]}"; do
   for size in "${dataset_sizes[@]}"; do
     for normalization in "${normalizations[@]}"; do
       for sdr in "${sdr_factors[@]}"; do
         echo "=================================================================================================="
-        echo "Running with: bkafi_criterion=$criterion, size=$size, vector_normalization=$normalization, SDR=$sdr"
+        # if eval_mode == blocking print the following
+        if [[ $eval_mode == "blocking" ]]; then
+          echo "Running blocking with: bkafi_criterion=$criterion, size=$size, vector_normalization=$normalization, SDR=$sdr"
+        else
+          echo "Running matching with: bkafi_criterion=$criterion, size=$size, vector_normalization=$normalization, SDR=$sdr"
+        fi
 
-        python main.py --bkafi_criterion $criterion --evaluation_mode $eval_mode --dataset_size_version $size \
-        --vector_normalization $normalization --sdr_factor $sdr
+        python main.py \
+          --bkafi_criterion $criterion \
+          --evaluation_mode $eval_mode \
+          --dataset_size_version $size \
+          --vector_normalization $normalization \
+          --sdr_factor $sdr
       done
     done
   done
